ab671038f15905bee547a669c8a1c7edd5defef3
rookgm <misharook27@gmail.com>
2025-05-13 23:39:46 +0300

    iter7

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,14 +1,17 @@
 package server
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/go-chi/chi/v5"
 	"github.com/rookgm/shortener/config"
 	"github.com/rookgm/shortener/internal/logger"
 	"github.com/rookgm/shortener/internal/random"
+	"go.uber.org/zap"
 	"io"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 var storage = map[string]string{}
@@ -52,6 +55,60 @@ func PostHandler(baseURL string) http.HandlerFunc {
 	}
 }
 
+type APIShortenReq struct {
+	URL string `json:"url"`
+}
+type APIShortenResp struct {
+	Result string `json:"result"`
+}
+
+func APIShortenHandler(baseURL string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger.Log.Debug("check Content-Type")
+		if ct := r.Header.Get("Content-Type"); ct != "" {
+			st := strings.ToLower(strings.TrimSpace(strings.Split(ct, ";")[0]))
+			if strings.Compare(st, "application/json") != 0 {
+				msg := "Content-Type is not application/json"
+				logger.Log.Debug(msg, zap.String("is", ct))
+				http.Error(w, msg, http.StatusUnsupportedMediaType)
+				return
+			}
+		}
+
+		var req APIShortenReq
+
+		logger.Log.Debug("decode request")
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			logger.Log.Debug("cannot decode JSON body", zap.Error(err))
+			http.Error(w, "bad request", http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+
+		// generating url alias
+		alias := random.RandString(6)
+		// put it storage
+		storage[alias] = string(req.URL)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+
+		var resp APIShortenResp
+		var err error
+
+		resp.Result, err = url.JoinPath(baseURL, alias)
+		if err != nil {
+			logger.Log.Debug("join url path", zap.Error(err))
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			logger.Log.Debug("cannot encode JSON body", zap.Error(err))
+			return
+		}
+	}
+}
+
 func Run(config *config.Config) error {
 	if config == nil {
 		return errors.New("config is nil")
@@ -63,6 +120,8 @@ func Run(config *config.Config) error {
 	router.Route("/", func(r chi.Router) {
 		router.Post("/", PostHandler(config.BaseURL))
 		router.Get("/{id}", GetHandler())
+		router.Post("/api/shorten", APIShortenHandler(config.BaseURL))
+
 	})
 
 	return http.ListenAndServe(config.ServerAddr, router)
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -123,8 +125,75 @@ func TestPostHandler(t *testing.T) {
 			defer res.Body.Close()
 			resBody, err := io.ReadAll(res.Body)
 			require.NoError(t, err)
-			assert.NotEmpty(t, resBody, "response body is empty")
+			assert.NotEmpty(t, resBody, "body body is empty")
 			assert.True(t, strings.HasPrefix(res.Header.Get("Content-Type"), string(test.want.contentType)), "Content-Type is not valid")
+		})
+	}
+}
+
+func TestApiShortenHandler(t *testing.T) {
+
+	type want struct {
+		code        int
+		contentType string
+		body        APIShortenResp
+	}
+
+	tests := []struct {
+		name        string
+		contentType string
+		body        APIShortenReq
+		want        want
+	}{
+		{
+			name:        "positive_test",
+			contentType: "application/json",
+			body:        APIShortenReq{URL: "https://practicum.yandex.ru/"},
+			want: want{
+				code:        http.StatusCreated,
+				contentType: "application/json",
+				body:        APIShortenResp{Result: "http://localhost:8080/"},
+			},
+		},
+		{
+			name:        "unsupported_media_type",
+			contentType: "text/plain",
+			want: want{
+				code:        http.StatusUnsupportedMediaType,
+				contentType: "text/plain",
+			},
+		},
+	}
+
+	handler := APIShortenHandler("http://localhost:8080")
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			body, _ := json.Marshal(test.body)
+			request := httptest.NewRequest(http.MethodPost, "/api/shorten", bytes.NewBuffer(body))
+			request.Header.Set("Content-Type", test.contentType)
+			w := httptest.NewRecorder()
+
+			handler(w, request)
+
+			res := w.Result()
+			assert.Equal(t, test.want.code, res.StatusCode)
+
+			defer res.Body.Close()
+
+			resBody, err := io.ReadAll(res.Body)
+
+			require.NoError(t, err)
+
+			assert.NotEmpty(t, resBody, "body body is empty")
+
+			assert.True(t, strings.HasPrefix(res.Header.Get("Content-Type"), string(test.want.contentType)), "Content-Type is not valid")
+
+			var resp APIShortenResp
+
+			json.Unmarshal(resBody, &resp)
+
+			assert.True(t, strings.HasPrefix(resp.Result, test.want.body.Result), "body result is not equal")
 		})
 	}
 }
